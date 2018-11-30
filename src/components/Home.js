74049e8d40218e005e6eb9e68ffbf178b84baa1c
EvanReinstein <edreins@gmail.com>
2018-11-30 14:24:35 -0800

    Adds Home page styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, Image, StyleSheet } from 'react-native';
 
 class Home extends Component {
   render() {
     return(
-      <View>
-        <Text>Home Page</Text>
-      </View>
+      <ScrollView>
+          <View style={styles.container}>
+            <Text style={styles.text}>Welcome to Crow Watch</Text>
+            <Image source={require('../../assets/crow.png')} />
+          </View>
+      </ScrollView>
     )
   }
 }
 
 export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20
+  },
+  text: {
+    fontSize: 40,
+  }
+})
