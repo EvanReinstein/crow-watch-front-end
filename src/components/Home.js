a3a3765f3b9ceb79b94a85eaecb23ccc85bac252
EvanReinstein <edreins@gmail.com>
2018-11-30 14:53:05 -0800

    Adds photo assets and styling for Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,17 @@ class Home extends Component {
     return(
       <ScrollView>
           <View style={styles.container}>
-            <Text style={styles.text}>Welcome to Crow Watch</Text>
+            <Text style={styles.textMainHeader}>Welcome to Crow Watch</Text>
             <Image source={require('../../assets/crow.png')} />
+          </View>
+          <View style={styles.container}>
+            <Text>About</Text>
+            <Text style={styles.textContainer}>After moving to Oakland, CA in the summer of 2017 I was lucky enough to
+              find an apartment with a beautiful deck and view of the Berkely, Oakland, and Montclair
+              Hills.  I got to work right away....
+            </Text>
+            <Image source={require('../../assets/jade.png')} style={styles.plantPhoto} />
+            <Image source={require('../../assets/lemonTrees.png')} style={styles.plantPhoto} />
           </View>
       </ScrollView>
     )
@@ -23,7 +32,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: 20
   },
-  text: {
+  textMainHeader: {
     fontSize: 40,
+  },
+  textContainer: {
+    width: 300,
+    marginTop: 20
+  },
+  plantPhoto: {
+    borderWidth: 1,
+    borderRadius: 15,
+    marginTop: 10,
+    marginBottom: 10
   }
 })
