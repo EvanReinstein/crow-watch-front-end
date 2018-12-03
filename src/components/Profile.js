aa050ec39f625cb383d9a39a1846ceb72b15af23
EvanReinstein <edreins@gmail.com>
2018-12-03 14:57:45 -0800

    Makes a successful GET fetch to S3 Bucket for a single Image

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import { Buffer } from 'buffer';
+import { s3Bucket } from '../../config/config';
 
 class Profile extends Component {
+  state = {
+    photos: []
+  }
+
+  componentWillMount() {
+    fetch(`${s3Bucket}Photo%3A+1`, {
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+    .then(res => {
+      let img = `data:image/png;base64,${res._bodyText}`;
+      this.setState({ photos: [img] });
+      console.log(img);
+    })
+    .catch(err => console.log(err))
+  }
+
   render() {
     return(
       <View>
         <Text>Welcome to Crow Watch</Text>
+        <View>
+        {this.state.photos.map((photo, i) => {
+          return <Image source={{uri: photo}} key={i} style={{width: 400, height: 400}}/>
+        })}
+        </View>
       </View>
     );
   }
