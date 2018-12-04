4a2b49e622d66ea0936bad8b6297bdf9c3781396
EvanReinstein <edreins@gmail.com>
2018-12-03 16:04:49 -0800

    Manually configures storage to connect to backend.  Obtains List of public images.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import { View, Text, Image } from 'react-native';
-import { Buffer } from 'buffer';
-import { s3Bucket } from '../../config/config';
+import { s3Bucket, identityPOOLID } from '../../config/config'
+
+// AWS Related Imports
+import Storage from '@aws-amplify/storage';
+import { S3Image } from 'aws-amplify-react-native';
+
+Storage.configure({
+  bucket: 'crowwatch',
+  region: 'us-west-2',
+  identityPoolId: identityPOOLID
+});
 
 class Profile extends Component {
   state = {
+    keys: [],
     photos: []
   }
 
   componentWillMount() {
+    Storage.list('')
+      .then(res => {
+        console.log(res);
+        let keys = res.map(photo => {
+          return photo.key;
+        });
+        this.setState({ keys });
+        console.log(this.state.keys);
+      })
+      .catch(err => console.log(err));
+
     fetch(`${s3Bucket}Photo%3A+1`, {
       headers: {
         'Accept': 'application/json'
@@ -17,7 +38,6 @@ class Profile extends Component {
     .then(res => {
       let img = `data:image/png;base64,${res._bodyText}`;
       this.setState({ photos: [img] });
-      console.log(img);
     })
     .catch(err => console.log(err))
   }
@@ -27,13 +47,15 @@ class Profile extends Component {
       <View>
         <Text>Welcome to Crow Watch</Text>
         <View>
-        {this.state.photos.map((photo, i) => {
-          return <Image source={{uri: photo}} key={i} style={{width: 400, height: 400}}/>
-        })}
+          {this.state.photos.map((photo, i) => {
+            return <Image source={{uri: photo}} key={i} style={{width: 400, height: 400}}/>
+          })}
         </View>
       </View>
     );
   }
 }
+
+
 
 export default Profile;
